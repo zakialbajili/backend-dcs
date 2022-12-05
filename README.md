81aed901392ac8afee95a2a5d1f9615821002846
tamir3232 <tamirrasydi@gmail.com>
2022-12-05 20:03:22 +0700

    nambah suplier

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 1. After PULL
 2. Create .env file, copy from .env.example
-3. Run npx primsa dev migrate --name init (on terminal or CMD)
+3. Run npx prisma migrate dev --name init (on terminal or CMD)
 4. Run npm install (on terminal or CMD)
--- routes.js
+++ routes.js
@@ -2,12 +2,14 @@ const UserController = require("./controllers/UserController")
 const RoleController = require("./controllers/RoleController")
 const UserRoleController = require("./controllers/UserRoleController")
 const AuthUserController = require("./controllers/AuthUserController")
+const SuplierController = require("./controllers/SuplierController")
 
 const _routes = [
     ['users', UserController],
     ['roles', RoleController],
     ['userRoles', UserRoleController],
-    ['', AuthUserController]
+    ['', AuthUserController],
+    ['sup', SuplierController],
 ]
 
 const routes = (app) => {
