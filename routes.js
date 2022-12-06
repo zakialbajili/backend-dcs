155e08f13e047f585feb17f552e7644cdc6d9aa1
tamir3232 <tamirrasydi@gmail.com>
2022-12-06 19:45:34 +0700

    fix api suplier dan pull semua dari gitlab

--- routes.js
+++ routes.js
@@ -4,25 +4,17 @@ const UserRoleController = require("./controllers/UserRoleController")
 const AuthUserController = require("./controllers/AuthUserController")
 const ReceiveController = require("./controllers/ReceiveController")
 const RackController = require("./controllers/RackController")
-<<<<<<< HEAD
 const SuplierController = require("./controllers/SuplierController")
-=======
 const BatchController = require("./controllers/BatchMaterialController")
->>>>>>> 71188524328a3517f4cbdf35aebc75db5338160b
-
 const _routes = [
     ['users', UserController],
     ['roles', RoleController],
     ['userRoles', UserRoleController],
     ['rack', RackController],
     ['receive', ReceiveController],
-<<<<<<< HEAD
+    ['batch', BatchController],
     ['', AuthUserController],
     ['sup', SuplierController]
-=======
-    ['batch', BatchController],
-    ['', AuthUserController]
->>>>>>> 71188524328a3517f4cbdf35aebc75db5338160b
 ]
 
 const routes = (app) => {
