da83b336e81aadfc09e3c908627e158cd3b426fe
zuhairajamt <zuhairajamt@gmail.com>
2022-12-08 17:22:47 +0700

    tambah route, module

---
+++ controllers/MaterialStockController.js
@@ -0,0 +1,26 @@
+const m$auth = require('../modules/material.module')
+const { Router } = require('express')
+const response = require('../helpers/response')
+const auth = require("../middleware/auth-middleware")
+
+const MaterialController = Router()
+
+MaterialController.get('/receives', auth, async (req, res) => {
+    const list = await m$auth.listMaterialReceives()
+
+    response.sendResponse(res, list)
+})
+
+// SW.C
+MaterialController.get('/in', auth, async (req, res) => {
+    const list = await m$auth.listMaterialReceives()
+
+    response.sendResponse(res, list)
+})
+MaterialController.get('/out', auth, async (req, res) => {
+    const list = await m$auth.listMaterialReceives()
+
+    response.sendResponse(res, list)
+})
+
+module.exports = MaterialController
---
+++ modules/FGstockOpname.module.js
@@ -0,0 +1,25 @@
+const prisma = require("../helpers/database")
+const Joi = require("joi")
+const list = require("../dummy/materialReceives.json")
+const jwt = require('jsonwebtoken')
+
+class _material {
+    listMaterialReceives = async () => {
+        try {
+            return {
+                status: true,
+                code: 200,
+                list
+            }
+        } catch (error) {
+            console.error('listRole role module Error: ', error);
+            return {
+                status: false,
+                error
+            }
+        }
+    }
+
+}
+
+module.exports = new _material
