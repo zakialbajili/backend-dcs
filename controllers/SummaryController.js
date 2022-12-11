641bb611d2a285d5dd26d97ca96999b212f9babb
zuhairajamt <zuhairajamt@gmail.com>
2022-12-11 19:58:33 +0700

    Dummy Summary DPA, Summary NG DONE

--- controllers/SummaryController.js
+++ controllers/SummaryController.js
@@ -1,14 +1,25 @@
-const m$auth = require('../modules/material.module')
+const m$auth = require('../modules/summary.module')
 const { Router } = require('express')
 const response = require('../helpers/response')
 const auth = require("../middleware/auth-middleware")
 
-const MaterialController = Router()
+const SummaryController = Router()
 
-MaterialController.get('/receives', auth, async (req, res) => {
-    const list = await m$auth.listMaterialReceives()
+SummaryController.get('/ng/', auth, async (req, res) => {
+    let materialId = req.query.materialid
+    let month = req.query.month
+
+    const list = await m$auth.listSummaryNG({ materialId: materialId, month: month })
 
     response.sendResponse(res, list)
 })
 
-module.exports = MaterialController
+SummaryController.get('/dpa', auth, async (req, res) => {
+    let month = req.query.month
+
+    const list = await m$auth.listSummaryDPA({month: month})
+
+    response.sendResponse(res, list)
+})
+
+module.exports = SummaryController
--- modules/summary.module.js
+++ modules/summary.module.js
@@ -1,18 +1,58 @@
 const prisma = require("../helpers/database")
 const Joi = require("joi")
-const list = require("../dummy/materialReceives.json")
+const listNG = require("../dummy/summaryNG.json")
+const listDPA = require("../dummy/summaryDPA.json")
 const jwt = require('jsonwebtoken')
+const { number } = require("joi")
+const dayjs = require("dayjs")
 
-class _material {
-    listMaterialReceives = async () => {
+// dayjs atau date and time
+// $queryRaw
+
+class _summary {
+    // Summary NG
+    listSummaryNG = async ({ materialId: materialId, month: month }) => {
         try {
-            return {
-                status: true,
-                code: 200,
-                list
+            if (materialId != undefined && month == undefined) {
+                const filteredList = listNG.data.filter(function (item) {
+                    return item.material_id == materialId;
+                });
+                return {
+                    status: true,
+                    code: 200,
+                    list: filteredList
+                }
+            } else if (materialId == undefined && month != undefined) {
+                const filteredList = listNG.data.filter(function (item) {
+                    const monthItem = item.created_at.substring(5, 7);
+                    return monthItem == month;
+                });
+                return {
+                    status: true,
+                    code: 200,
+                    list: filteredList
+                }
+            } else if ((materialId != undefined && month != undefined)) {
+                const filteredList = listNG.data.filter(function (item) {
+                    const monthItem = item.created_at.substring(5, 7);
+                    return monthItem == month && item.material_id == materialId;
+                });
+                return {
+                    status: true,
+                    code: 200,
+                    list: filteredList
+                }
             }
+            else {
+                return {
+                    status: true,
+                    code: 200,
+                    list: listNG
+                }
+            }
+
         } catch (error) {
-            console.error('listRole role module Error: ', error);
+            console.error('list summary NG module Error: ', error);
             return {
                 status: false,
                 error
@@ -20,6 +60,35 @@ class _material {
         }
     }
 
+    // Summary DPA
+    listSummaryDPA = async ({ month: month }) => {
+        try {
+            if (month != undefined) {
+                const filteredList = listDPA.data.filter(function (item) {
+                    const monthItem = item.created_at.substring(5, 7);
+                    return monthItem == month;
+                });
+                return {
+                    status: true,
+                    code: 200,
+                    list: filteredList
+                }
+            } else {
+                return {
+                    status: true,
+                    code: 200,
+                    list: listDPA
+                }
+            }
+        }
+        catch (error) {
+            console.error('list summary NG module Error: ', error);
+            return {
+                status: false,
+                error
+            }
+        }
+    }
 }
 
-module.exports = new _material
+module.exports = new _summary
