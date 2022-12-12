117e421bbd0896884038be55b0d76a211086e968
zakialbajili <zakyalbajili10@gmail.com>
2022-12-12 12:34:52 +0700

    add upload work order and list wok order

--- controllers/history_work_orderController.js
+++ controllers/history_work_orderController.js
@@ -4,15 +4,14 @@ const { Router } = require('express')
 const response = require('../helpers/response')
 const uploadFile = require ('../middleware/upload-files')
 const hwoController = Router()
+hwoController.get('/', async (req, res) => {
+   const data = await m$hwo.listwo()
+    response.sendResponse(res, data)
+})
 
-    hwoController.post('/', uploadFile.single('name_file'), async (req, res) => {
-        console.log(req.file)
-        const data = await m$hwo.createhwo({
-            name_file:req.file.filename,
-        })
-    
-        response.sendResponse(res, data)
-    })
-    
-    
-    module.exports = hwoController
+hwoController.post('/', uploadFile.single('file'), async (req, res) => {
+
+    const data = await m$hwo.upload_wo(req.file)
+    response.sendResponse(res, data)
+})
+module.exports = hwoController
--- modules/history_work_order.module.js
+++ modules/history_work_order.module.js
@@ -1,12 +1,18 @@
 const prisma = require("../helpers/database")
 const Joi = require("joi")
-
-class _hwo {
-
-    createhwo = async (body) => {
-        try {
+const XLSX = require ('xlsx')
+class _wo {
+    upload_wo = async (body)=>{
+        try{
             const schema = Joi.object({
-                name_file: Joi.string().required(),
+                fieldname: Joi.string().required(),
+                originalname: Joi.string().required(),
+                encoding: Joi.string().required(),
+                mimetype: Joi.string().required(),
+                destination: Joi.string().required(),
+                filename: Joi.string().required(),
+                path: Joi.string().required(),
+                size: Joi.number().required(),
             }).options({ abortEarly: false })
 
             const validation = schema.validate(body)
@@ -19,20 +25,102 @@ class _hwo {
                     error: errorDetails.join(', ')
                 }
             }
-            console.log(body)
-            const add = await prisma.history_work_order.create({
+            var workbook = XLSX.readFile(body.path)
+            let worksheets = {}
+            for (const sheetName of workbook.SheetNames){
+                worksheets[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
+            }
+            const inputhwo= await prisma.history_work_order.create({
+                data:{
+                    name_file:body.filename
+                }
+            })
+            let add
+            for (let i = 0; i< worksheets.Sheet1.length; i++){
+                //console.log(worksheets.Sheet1)
+                let part_name=worksheets.Sheet1[i].part_name
+                let no_work_order=worksheets.Sheet1[i].no_work_order
+                let customer=worksheets.Sheet1[i].customer
+                let quantity_perbox=worksheets.Sheet1[i].quantity_perbox
+                let total_order=worksheets.Sheet1[i].total_order
+                let total_box=total_order/quantity_perbox
+                let supplier_id=worksheets.Sheet1[i].supplier
+                
+                let supplier = await prisma.supplier.findFirst({
+                    where: {
+                        name: supplier_id
+                    },
+                    select: {
+                        id: true
+                    }
+                })
+                //console.log(supplier.id)
+                if (!supplier) {
+                    return {
+                        status: false,
+                        code: 401,
+                        error: 'Supplier not found'
+                    }
+                }    
+                 add = await prisma.work_order.create({
+                    data: {
+                        id_file: 1,
+                        part_name: part_name,
+                        no_work_order: no_work_order,
+                        customer : customer,
+                        quantity_perbox: quantity_perbox,
+                        total_order: total_order,
+                        total_box: total_box,
+                        supplier_id: supplier.id
+                     }
+                    })
+                
+                console.log(add)
+                }
+                for(let j=0; j<total_box; j++){
+
+                }
+                return {
+                    status: true,
+                    code: 201,
+                    data: add
+                }
+            /*const add = await prisma.history_work_order.create({
                 data: {
                     name_file: body.name_file
                 }
-            })
-
-            return {
-                status: true,
-                code: 201,
-                data: add
-            }
+            })*/
         } catch (error) {
             console.error('historyWorkOrder hwo module Error:', error);
+            return {
+                status: false,
+                error
+            }
+        }
+    }
+    listwo=async ()=>{
+        try{
+            const list = await prisma.work_order.findMany({
+                select:{
+                    id: true,
+                    part_name: true,
+                    no_work_order:true,
+                    customer:true,
+                    quantity_perbox: true,
+                    total_order: true,
+                    total_box: true,
+                    supplier: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            })
+            return {
+                status: true,
+                data: list
+            }
+        }catch(error){
             return {
                 status: false,
                 error
@@ -42,4 +130,4 @@ class _hwo {
 
     
 }
-module.exports = new _hwo()
+module.exports = new _wo()
