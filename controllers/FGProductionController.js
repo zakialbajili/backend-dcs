fcd974ceb25b254f17cdc4fd022505e03967d645
MHabilArifin <mhabilarifin1414@gmail.com>
2022-12-14 16:27:02 +0700

    fgproductiondummy

--- controllers/FGProductionController.js
+++ controllers/FGProductionController.js
@@ -1,20 +1,20 @@
-const m$auth = require('../modules/material.module')
-const { Router } = require('express')
-const response = require('../helpers/response')
-const auth = require("../middleware/auth-middleware")
+const m$auth = require("../modules/FGProduction.module");
+const { Router } = require("express");
+const response = require("../helpers/response");
+const auth = require("../middleware/auth-middleware");
 
-const MaterialController = Router()
+const FGProductionController = Router();
 
 // SW.C
-MaterialController.get('/in', auth, async (req, res) => {
-    const list = await m$auth.listMaterialReceives()
+FGProductionController.get("/in", auth, async (req, res) => {
+  const list = await m$auth.listFGProduction();
 
-    response.sendResponse(res, list)
-})
-MaterialController.get('/out', auth, async (req, res) => {
-    const list = await m$auth.listMaterialReceives()
+  response.sendResponse(res, list);
+});
+FGProductionController.get("/out", auth, async (req, res) => {
+  const list = await m$auth.listFGProduction();
 
-    response.sendResponse(res, list)
-})
+  response.sendResponse(res, list);
+});
 
-module.exports = MaterialController
+module.exports = FGProductionController;
--- modules/FGProduction.module.js
+++ modules/FGProduction.module.js
@@ -1,25 +1,24 @@
-const prisma = require("../helpers/database")
-const Joi = require("joi")
-const list = require("../dummy/materialReceives.json")
-const jwt = require('jsonwebtoken')
+const prisma = require("../helpers/database");
+const Joi = require("joi");
+const list = require("../dummy/fgproduction.json");
+const jwt = require("jsonwebtoken");
 
-class _material {
-    listMaterialReceives = async () => {
-        try {
-            return {
-                status: true,
-                code: 200,
-                list
-            }
-        } catch (error) {
-            console.error('listRole role module Error: ', error);
-            return {
-                status: false,
-                error
-            }
-        }
+class _FGProduction {
+  listFGProduction = async () => {
+    try {
+      return {
+        status: true,
+        code: 200,
+        list,
+      };
+    } catch (error) {
+      console.error("listRole role module Error: ", error);
+      return {
+        status: false,
+        error,
+      };
     }
-
+  };
 }
 
-module.exports = new _material
+module.exports = new _FGProduction();
