3d8054955a4852697673dba56243494ceee39ab1
Yudhi Kusuma <yudhie123@gmail.com>
2022-12-16 13:08:46 +0700

    feat: handle PORT and welcome API

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(FileUpload())
+
+app.get('/', async (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to Backend SI DCS'
+    })
+})
 
 routes(app)
 
